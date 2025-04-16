d46def605480c95fd45699c14636f20a7eee423c
Honey-Sharma <honeysharmaemail@gmail.com>
2025-04-16 13:04:20 +0530

    -Improve homepage navbar. -Add index.css and link it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const navbarElm = document.querySelector(".navbar");
+const navBarOriginalOffset = navbarElm.offsetTop;
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navBarOriginalOffset) {
+    navbarElm.style.position = "fixed";
+    navbarElm.style.top = "0";
+    navbarElm.style.left = "0";
+    navbarElm.style.right = "0";
+  } else {
+    navbarElm.style.position = "static";
+  }
+});
